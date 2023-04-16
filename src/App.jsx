eb130a138ae1ae78fc5b06c964a9348ff37b77aa
Lorena.Borrull <lorena.borrull@basetis.com>
2023-04-17 01:01:38 +0200

    resolved conflicts in the main branch in  nav branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import nav from './components/nav'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+from '/vite.svg'
 import './App.css'
 
 function App() {
